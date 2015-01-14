e40766336dc7c7c4334d7930ab1ba38f375f415f
michaeljosephrosenthal <rosenthalm93@gmail.com>
2015-01-14 15:49:46 +0700

    reducing startup time

--- app/views.py
+++ app/views.py
@@ -17,22 +17,24 @@ def save_words(f, lst):
     f.write(json.dumps(lst))
     f.truncate()
 
-with open("wordlist.txt", "r") as thai_words, open('wordlist.json', 'r+') as json_file:
-    translated = json.load(json_file)
-    new_wordlist = [
-            word.rstrip().decode('utf-8')
-            for word in thai_words.readlines()
-            if word.rstrip().decode('utf-8')
-            not in [ w["thai"] for w in translated ]
-        ]
-    for word in new_wordlist:
-        translated.append({
-            "thai": word,
-            "eng": trans(word)
-            })
-    save_words(json_file, translated)
+def init_list():
+    with open("wordlist.txt", "r") as thai_words, open('wordlist.json', 'r+') as json_file:
+        translated = json.load(json_file)
+        new_wordlist = [
+                word.rstrip().decode('utf-8')
+                for word in thai_words.readlines()
+                if word.rstrip().decode('utf-8')
+                not in [ w["thai"] for w in translated ]
+            ]
+        for word in new_wordlist:
+            translated.append({
+                "thai": word,
+                "eng": trans(word)
+                })
+        save_words(json_file, translated)
+        return translated
     
 
 @route('/')
 def home():
-    return template('home.html', wordlist=translated)
+    return template('home.html', wordlist=init_list())
