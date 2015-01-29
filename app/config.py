ff0346f6b45564448de48e5e6c2550ec07ff9a13
michaeljosephrosenthal <rosenthalm93@gmail.com>
2015-01-30 03:21:31 +0700

    init_db in env

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ DEV = os.environ.get("app_env", "Prod") == "Dev"
 HOST = '0.0.0.0'
 PORT = os.environ.get("PORT", 5000)
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://mjr:@localhost/type_flashcards")
-INIT_DB = False
+INIT_DB = os.environ.get("INIT_DB", False)
 LOG_LEVEL = logging.DEBUG
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=LOG_LEVEL)
