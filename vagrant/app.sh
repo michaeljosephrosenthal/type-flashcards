28936bc2179b9042984b31c78ef87d1dc7a662e1
michaeljosephrosenthal <michael.the.awesome@gmail.com>
2015-03-22 20:22:35 +0700

    stamp head in app

--- vagrant/app.sh
+++ vagrant/app.sh
@@ -7,6 +7,7 @@ cd /type-flashcards/
 pip install -r requirements.txt
 cp vagrant/dev.env .env
 echo "CREATE DATABASE type_flashcards;" | sudo -u postgres psql
+
 honcho start web -e vagrant/init-db.env
 honcho start init_dev_db
-
+honcho run alembic stamp head
